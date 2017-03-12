4860a5c043ab1167f6c5c9f0fe2709b65142b8b4
Cythilya Tang <cythilya@gmail.com>
2017-03-12 14:19:12 +0800

    modify uuid method by adding high-precision timer if available

--- js/script.js
+++ js/script.js
@@ -99,7 +99,15 @@ var app = new Vue({
       this.filter = filter;
     },
     _uuid: function() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {var r = Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;return v.toString(16);});
+      var d = Date.now();
+      if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+          d += performance.now(); //use high-precision timer if available
+      }
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+          var r = (d + Math.random() * 16) % 16 | 0;
+          d = Math.floor(d / 16);
+          return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      });
     },
     _getTodos: function(isCompleted) {
       return this.todos.filter(function(value) {
