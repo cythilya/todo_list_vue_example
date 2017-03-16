b0e30fb85a6bc4433df8908c9da1dce6bdcc74ca
Cythilya Tang <cythilya@gmail.com>
2017-03-16 21:33:25 +0800

    update mock data

--- js/script.js
+++ js/script.js
@@ -35,19 +35,19 @@ var app = new Vue({
     todos: {
       "a5436691-350c-4ed0-862e-c8abc8509a4a": {
         "uuid": "a5436691-350c-4ed0-862e-c8abc8509a4a",
-        "text": "買一本好書 a",
+        "text": "買一本好書",
         "isCompleted": false,
         "isEdit": false
       },
       "a98bf666-a710-43b2-81b2-60c68ec4688d": {
         "uuid": "a98bf666-a710-43b2-81b2-60c68ec4688d",
-        "text": "打電話給小明 b",
+        "text": "打電話給小明",
         "isCompleted": true,
         "isEdit": false
       },
       "452ef417-033d-48ff-9fec-9d686c105dce": {
         "uuid": "452ef417-033d-48ff-9fec-9d686c105dce",
-        "text": "寫一篇文章 c",
+        "text": "寫一篇文章",
         "isCompleted": false,
         "isEdit": false
       }
